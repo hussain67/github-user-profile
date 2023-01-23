c906bc51e94d9a311d15850e97946fd532505ef7
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2023-01-23 16:20:34 +0000

    Router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Dashbord from "./pages/dashboard/Dashbord";
+import Login from "./pages/login/Login";
+import Error from "./pages/error/Error";
 
 function App() {
-	return <div className="App">Bismillah</div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={<Dashbord />}
+					/>
+					<Route
+						path="/login"
+						element={<Login />}
+					/>
+					<Route
+						path="*"
+						element={<Error />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
